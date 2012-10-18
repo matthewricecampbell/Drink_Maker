0120239a68ac7308e6a3db71c6ec378d26426b12
Matt Campbell <matthewricecampbell@gmail.com>
2012-10-18 15:16:38 -0500

    V3

--- lib/Cocktail_Companion.rb
+++ lib/Cocktail_Companion.rb
@@ -22,44 +22,75 @@ class Cocktail_Maker
     
     def ingredient_printer_getter
         ingredients['spirits'].each {|key, value| puts "#{key} #{value} "}
-        puts 'What spirits do you have on hand?'
+        @stdout.puts 'What spirits do you have on hand?'
         @spirits = gets.split.map {|ingredient| ingredients['spirits'][ingredient.to_i] }
         
         ingredients['mixers'].each {|key, value| puts "#{key} #{value} "}
-        puts 'What mixers do you have on hand?'
+        @stdout.puts 'What mixers do you have on hand?'
         @mixers = gets.split.map {|ingredient| ingredients['mixers'][ingredient.to_i] }
         
         ingredients['fruits_juices'].each {|key, value| puts "#{key} #{value}" }
-        puts 'What fruits and juices do you have on hand?'
+        @stdout.puts 'What fruits and juices do you have on hand?'
         @fruits_juices = gets.split.map {|ingredient| ingredients['fruits_juices'][ingredient.to_i] }
         
         ingredients['liqueurs_bitters'].each {|key, value| puts "#{key} #{value}" }
-        puts 'What liqueurs and bitters do you have on hand?'
+        @stdout.puts 'What liqueurs and bitters do you have on hand?'
         @liqueurs_bitters = gets.split.map {|ingredient| ingredients['liqueurs_bitters'][ingredient.to_i] }
         
         ingredients['syrups'].each {|key, value| puts "#{key} #{value}"}
-        puts 'What syrups do you have on hand?'
+        @stdout.puts 'What syrups do you have on hand?'
         @syrups = gets.split.map {|ingredient| ingredients['syrups'][ingredient.to_i] }
         
         ingredients['pantry_items'].each {|key, value| puts "#{key} #{value}"}
-        puts 'What pantry items do you have on hand?'
+        @stdout.puts 'What pantry items do you have on hand?'
         @pantry_items = gets.split.map {|ingredient| ingredients['pantry_items'][ingredient.to_i] }
     end
 #52 ex: if any @spirits ie:["absinthe" or "rum"] are included in a row return true and add that row to an array and puts out
+    def spirit_found_in_row
+        @spirits.any? {|spirit| @row_string.include?(spirit)}
+    end
+
+    def mixer_found_in_row
+        @mixers.any? {|mixer| @row_string.include?(mixer)}
+    end
+
+    def fruit_juices_found_in_row
+        @fruits_juices.any? {|fruits_juices| @row_string.include?(fruits_juices)}
+    end
+
+    def syrup_found_in_row
+        @syrups.any? {|syrups| @row_string.include?(syrups)}
+    end
+
+    def liquers_bitters_found_in_row
+         @liqueurs_bitters.any? {|liqueurs_bitters| @row_string.include?(liqueurs_bitters)}
+    end
+
+    def pantry_items_found_in_row
+        @pantry_items.any? {|pantry_items| @row_string.include?(pantry_items)}
+    end
+
     def recipe_finder
-        CSV.open("drink_recipes_database.csv", "r").each do |row|
-            drink_recipe_array = []
-            row_string = row.join("")
-            #if this true shouldn't the statement end? It seems to be going through elsif's
-            if @spirits.any? {|spirit| row_string.include?(spirit)} && @fruits_juices.any? {|fruits_juices| row_string.include?(fruits_juices)} && @liqueurs_bitters.any? {|liqueurs_bitters| row_string.include?(liqueurs_bitters)} && @syrups.any? {|syrups| row_string.include?(syrups)}
-                drink_recipe_array += row
-                puts row 
-            elsif (@spirits.any? {|spirit| row_string.include?(spirit)} && @mixers.any? {|mixer| row_string.include?(mixer)}) || (@spirits.any? {|spirit| row_string.include?(spirit)} && @fruits_juices.any? {|fruits_juices| row_string.include?(fruits_juices)}) || (@spirits.any? {|spirit| row_string.include?(spirit)}&& @syrups.any? {|syrups| row_string.include?(syrups)}) || (@spirits.any? {|spirit| row_string.include?(spirit)} && @pantry_items.any? {|pantry_items| row_string.include?(pantry_items)})
-                drink_recipe_array += row
-                puts row
-            elsif @spirits.any? {|spirit| row_string.include?(spirit)} #need to do: only put 3 recipes
-                drink_recipe_array += row
-                puts row
+        drink_recipe_array = []#create an empty array
+        CSV.open("drink_recipes_database.csv", "r").each do |row|#open csv file/push each row though one at a time
+            @row_string = row.join("")#create instance of row_string that is row.join("") 
+            if spirit_found_in_row #if spirit
+                if (fruit_juices_found_in_row && liquers_bitters_found_in_row && syrup_found_in_row) #if spirit & fj's & lb's & sy's match
+                    drink_recipe_array << {:score => 4, :row => row} #shovel matching rows into the drink_recipe_array with a score of 4
+                elsif (mixer_found_in_row || #if spirit & mixer
+                       fruit_juices_found_in_row || # OR if spirit & fj
+                       syrup_found_in_row || # OR if spirit & syrup
+                       pantry_items_found_in_row) #OR if spirit & pantry_items
+                    drink_recipe_array << {:score => 2, :row => row} #which ever matches shovel into the array
+                else
+                    drink_recipe_array << {:score => 1, :row => row} #else shovel spirit into array
+                end
+            end
+        end
+        max_score = drink_recipe_array.map { |recipe| recipe[:score] }.max #mapping over array and find the max score (ex. =>4)
+        drink_recipe_array.each do |recipe| #for each row (ie recipe) check to see if it matches the max score 
+            if recipe[:score] == max_score #if they are equal
+                @stdout.puts recipe[:row] # puts to stdout
             end
         end
     end
--- test/test.rb
+++ test/test.rb
@@ -12,34 +12,40 @@ require 'stringio'
     
    def test_02_mixers
     	c = Cocktail_Maker.new 
-		assert_equal 'Club soda' , c.ingredients['mixers'][1] 
+		assert_equal 'Club Soda' , c.ingredients['mixers'][1] 
    end
    
    def test_03_fruits_juices
     	c = Cocktail_Maker.new 
-		assert_equal 'apple cider' , c.ingredients['fruits_juices'][1] 
+		assert_equal 'Apple Cider' , c.ingredients['fruits_juices'][1] 
    end
    
    def test_04_liqueurs_bitters
      c = Cocktail_Maker.new
-     assert_equal 'absinthe' , c.ingredients['liqueurs_bitters'][1] 
+     assert_equal 'Absinthe' , c.ingredients['liqueurs_bitters'][1] 
    end
    
    def test_05_syrups
     c = Cocktail_Maker.new
-     assert_equal 'cane syrup' , c.ingredients['syrups'][1] 
+     assert_equal 'Cane Syrup' , c.ingredients['syrups'][1] 
    end 
    
    def test_06_syrups
     c = Cocktail_Maker.new
-     assert_equal 'allspice' , c.ingredients['pantry_items'][1] 
+     assert_equal 'Allspice' , c.ingredients['pantry_items'][1] 
    end
 
-   def test_07
+   def test_07_a_instructions
     stdout = StringIO.new()
-    cocktail = Cocktail_Maker.new([], stdout)
+    cocktail = Cocktail_Maker.new(stdout)
     cocktail.instructions
-    assert_equal( stdout.string, "Welcome to Cocktail Companion!\n Please tell me the ingredients you have on hand in this format (example entry: 1 2 3) then press return to move thorugh the categories!\n")
+    assert_equal( stdout.string, "Welcome to Cocktail Companion!\n Please tell me the ingredients you have on hand in this format example entry:\n 1 2 3\n Then press return to move thorugh the categories!\n")
    end
 
+   def test_07_b_ingredient_printer_getter
+    stdout = StringIO.new()
+    cocktail = Cocktail_Maker.new(stdout)
+    cocktail.ingredient_printer_getter
+    assert_equal( stdout.string, "What spirits do you have on hand?\nWhat mixers do you have on hand?\nWhat fruits and juices do you have on hand?\nWhat liqueurs and bitters do you have on hand?\nWhat syrups do you have on hand?\nWhat pantry items do you have on hand?\n")
+   end
 end
